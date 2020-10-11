d84988926512c8f4bb162a275defb1293d3050eb
VanillaBrooks <brooks@karlik.org>
2020-10-11 16:00:48 -0700

    refactor graph, add upload menu

---
+++ src/file_upload.js
@@ -0,0 +1,69 @@
+import {useDropzone} from 'react-dropzone'
+import json_data from './streams.json';
+import React, {useCallback} from 'react';
+
+function JsonUpload(props) {
+	const onDrop = useCallback(acceptedFiles => {
+		let reader = new FileReader();
+		reader.readAsText(acceptedFiles[0])
+
+		reader.onload = function() {
+			props.onUpload(JSON.parse(reader.result));
+  		};
+  	}, [props])
+
+  	const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+	const onClick = function(event) {
+		props.onUpload(json_data)
+	}
+ 
+	// TODO: the padding in here is bad but ive spent like an hour trying to vertically
+	// center in bootstrap so it is what it is
+  	return (
+		<div className="container">
+			<div style={{"padding":"20% 0"}}/>
+			<div className="row justify-content-center">
+				<div className="col-5 align-self-center shadow shadow-lg rounded">
+					<div {...getRootProps(
+						{
+							style: {
+								"min-height":"100px",
+								"border-style":"dashed",
+								"border-radius":"3px",
+								"border-width":"1px"
+							},
+							class: "p-2"
+						}
+					)
+					}>
+						{/* This is the drag-to-upload a file*/}
+						<input {...getInputProps()} />
+						{
+  	  				  	  isDragActive ?
+  	  				  	    <p className="text-center">Drop Here</p> :
+  	  				  	    <p className="text-center">Drag A Parsing Result to View</p>
+						}
+
+  	  				</div>
+
+					<div class="row p-2">
+						<div class="col">
+							<p className="text-center">or:</p>
+						</div>
+					</div>
+
+					{/* OTHERWISE: we can load a default file graph*/}
+					<div class="row">
+						<div class="col align-self-center">
+							<button className="btn btn-primary" onClick={onClick}>Load Example Graph</button>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+  	)
+}
+
+export default JsonUpload;
---
+++ src/graph.js
@@ -0,0 +1,177 @@
+import React from 'react';
+
+import './App.css';
+import { Graph } from 'react-d3-graph';
+
+class CustomGraph extends React.Component {
+	constructor(props) {
+		super(props)
+
+		// graph payload (with minimalist structure)
+		const nodes = props.graph_json.nodes.map(x => {
+			return {id: x.self_subroutine_name}
+		})
+		const edges = props.graph_json.edges.map(x => {
+			let map = {
+				source: x.self_subroutine_name,
+				target: x.called_subroutine_name
+			};
+			return map
+		})
+		console.log("nodes");
+		console.log(nodes);
+		console.log("edges");
+		console.log(edges);
+
+		const data = {
+			nodes: nodes,
+			links: edges
+		};
+		
+		// the graph configuration, you only need to pass down properties
+		// that you want to override, otherwise default ones will be used
+		const myConfig = make_config();
+		this.state = {data: data, config: myConfig};
+
+	}
+
+	onZoomChange(prevZoom, newZoom) {
+		this.setState({currentZoom: newZoom});
+	}
+	
+	render() {
+		const graph = <Graph
+		     id='graph-id' // id is mandatory, if no id is defined rd3g will throw an error
+		     data={this.state.data}
+		     config={this.state.config}
+			 onZoomChange={this.onZoomChange}
+		     //onClickGraph={onClickGraph}
+		     //onClickNode={onClickNode}
+		     //onDoubleClickNode={onDoubleClickNode}
+		     //onRightClickNode={onRightClickNode}
+		     //onClickLink={onClickLink}
+		     //onRightClickLink={onRightClickLink}
+		     //onMouseOverNode={onMouseOverNode}
+		     //onMouseOutNode={onMouseOutNode}
+		     //onMouseOverLink={onMouseOverLink}
+		     //onMouseOutLink={onMouseOutLink}
+			/>
+		console.log(graph)
+		return graph
+	}
+
+
+}
+
+// Callback to handle click on the graph.
+// @param {Object} event click dom event
+const onClickGraph = function(event) {
+     //window.alert('Clicked the graph background');
+};
+
+const onClickNode = function(nodeId) {
+     //window.alert('Clicked node ${nodeId}');
+};
+
+const onDoubleClickNode = function(nodeId) {
+     //window.alert('Double clicked node ${nodeId}');
+};
+
+const onRightClickNode = function(event, nodeId) {
+     //window.alert('Right clicked node ${nodeId}');
+};
+
+const onMouseOverNode = function(nodeId) {
+     //window.alert(`Mouse over node ${nodeId}`);
+};
+
+const onMouseOutNode = function(nodeId) {
+     //window.alert(`Mouse out node ${nodeId}`);
+};
+
+const onClickLink = function(source, target) {
+     //window.alert(`Clicked link between ${source} and ${target}`);
+};
+
+const onRightClickLink = function(event, source, target) {
+     //window.alert('Right clicked link between ${source} and ${target}');
+};
+
+const onMouseOverLink = function(source, target) {
+     //window.alert(`Mouse over in link between ${source} and ${target}`);
+};
+
+const onMouseOutLink = function(source, target) {
+     //window.alert(`Mouse out link between ${source} and ${target}`);
+};
+
+const onNodePositionChange = function(nodeId, x, y) {
+     //window.alert(`Node ${nodeId} moved to new position x= ${x} y= ${y}`);
+};
+
+const make_config = function() {
+	return {
+	  "automaticRearrangeAfterDropNode": false,
+	  "collapsible": false,
+	  "directed": true,
+	  "focusAnimationDuration": 0.75,
+	  "focusZoom": 1,
+	  "height": 1000,
+	  "highlightDegree": 1,
+	  "highlightOpacity": 0.2,
+	  "linkHighlightBehavior": false,
+	  "maxZoom": 8,
+	  "minZoom": 0.1,
+	  "nodeHighlightBehavior": true,
+	  "panAndZoom": false,
+	  "staticGraph": false,
+	  "staticGraphWithDragAndDrop": false,
+	  "width": 1600,
+	  "d3": {
+	    "alphaTarget": 0.05,
+	    "gravity": -2000,
+	    "linkLength": 400,
+	    "linkStrength": .1,
+	    "disableLinkForce": false
+	  },
+	  "node": {
+	    "color": "#d3d3d3",
+	    "fontColor": "black",
+	    "fontSize": 20,
+	    "fontWeight": "normal",
+	    "highlightColor": "red",
+	    "highlightFontSize": 30,
+	    "highlightFontWeight": "bold",
+	    "highlightStrokeColor": "SAME",
+	    "highlightStrokeWidth": 1.5,
+	    "labelProperty": "name",
+	    "mouseCursor": "pointer",
+	    "opacity": 1,
+	    "renderLabel": true,
+	    "size": 450,
+	    "strokeColor": "none",
+	    "strokeWidth": 1.5,
+	    //"svg": "",
+	    "symbolType": "circle"
+	  },
+	  "link": {
+	    "color": "#d3d3d3",
+	    "fontColor": "red",
+	    "fontSize": 10,
+	    "fontWeight": "normal",
+	    "highlightColor": "blue",
+	    "highlightFontSize": 8,
+	    "highlightFontWeight": "bold",
+	    "mouseCursor": "pointer",
+	    "opacity": 1,
+	    "renderLabel": false,
+	    "semanticStrokeWidth": false,
+	    "strokeWidth": 2,
+	    "markerHeight": 6,
+	    "markerWidth": 6,
+		//"type": "CURVE_SMOOTH"
+	  }
+	}
+}
+
+export default CustomGraph;
